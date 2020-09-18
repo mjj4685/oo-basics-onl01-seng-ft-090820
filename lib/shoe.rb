7bbcd483b4a4b5e6ec330678a3235ed429b763ea
MJ Johnson <mjohnson4685@gmail.com>
2020-09-18 22:10:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe                 # Make your shoe class here!
+    attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble()
+    self.condition = "new"
+    puts "Your shoe is as goo as new!"
+  end
+
+end
+
+Shoe.new("Nike")
